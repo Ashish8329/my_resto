3bfd66e5fd9363bf210738859e40614a42ad6d6d
Ashish auti <ashishauti123@gmail.com>
2026-01-09 09:56:48 +0000

    fix : add filter to list menu based on category

--- backend/my_resto/menus/views.py
+++ backend/my_resto/menus/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import MenuCategory, MenuItem
 from .serializers import MenuCategorySerializer, MenuItemSerializer
@@ -9,6 +10,13 @@ class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = super().get_queryset()
+        category_id =  self.request.query_params.get('category')
+        if category_id:
+            queryset = queryset.filter(menu_category=category_id)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class MenuCategoryViewSet(viewsets.ModelViewSet):
     queryset = MenuCategory.objects.all()

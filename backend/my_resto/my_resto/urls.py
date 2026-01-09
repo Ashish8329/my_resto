f9f5ee5ac39ddd15268043e0ae215a2082deae4a
Ashish auti <ashishauti123@gmail.com>
2026-01-09 05:15:11 +0000

    feat : media file setup

--- backend/my_resto/my_resto/urls.py
+++ backend/my_resto/my_resto/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path(
@@ -32,3 +34,9 @@ urlpatterns = [
     ),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )

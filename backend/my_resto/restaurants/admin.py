e30be8eaee6230313731b6a4f33b6674bfe42085
Ashish auti <ashishauti123@gmail.com>
2026-01-09 07:54:02 +0000

    feat : add restotable for admin page customization

--- backend/my_resto/restaurants/admin.py
+++ backend/my_resto/restaurants/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from base.base_admin import BaseAdmin
-from .models import Restaurant
+from .models import Restaurant, RestoTable
 
 @admin.register(Restaurant)
 class RestaurantAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(RestoTable)
+class RestaurantTableAdmin(BaseAdmin):
     pass

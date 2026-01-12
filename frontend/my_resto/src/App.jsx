48096fecf656d4ef321820658c8ca2009f94f1a0
Ashish auti <ashishauti123@gmail.com>
2026-01-12 10:20:17 +0000

    feat : add react router

--- frontend/my_resto/src/App.jsx
+++ frontend/my_resto/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Menu from './components/user/Menu'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
-      <Menu />
+      <Routes>
+        <Route path='/menu' element={<Menu />} />
+      </Routes>
     </>
   )
 }

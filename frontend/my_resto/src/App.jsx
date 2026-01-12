5270a016312a078b27e708a7a6f0bed30bfcaed9
Ashish auti <ashishauti123@gmail.com>
2026-01-12 09:53:16 +0000

    fix : add react router

--- frontend/my_resto/src/App.jsx
+++ frontend/my_resto/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Routes>
+      <Route path='/' elements={<h1>Home</h1>}/>
+      {/* <Route path="/orders/:id" element={<OrderDetails />} /> */}
+
+    </Routes>
       
     </>
   )

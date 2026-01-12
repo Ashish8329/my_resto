eb4d5424acde8be7fa76a7002173f2cc5cd0757b
Ashish auti <ashishauti123@gmail.com>
2026-01-12 10:12:55 +0000

    fix :

--- frontend/my_resto/src/App.jsx
+++ frontend/my_resto/src/App.jsx
@@ -2,19 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import Menu from './components/user/Menu'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Routes>
-      <Route path='/' elements={<h1>Home</h1>}/>
-      {/* <Route path="/orders/:id" element={<OrderDetails />} /> */}
-
-    </Routes>
-      
+      <h1>hello</h1>
+      <Menu />
     </>
   )
 }

40c808138fdc78a804e96a9b7a0e016c745a9019
Ashish auti <ashishauti123@gmail.com>
2026-01-15 09:39:20 +0000

    feat : add chef get order

--- backend/my_resto/users/urls.py
+++ backend/my_resto/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import scan_qr
+from .views import scan_qr, get_chef_orders
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,6 +7,7 @@ router = DefaultRouter()
 # router.register(f'scan', , basename='scan-qr')
 
 urlpatterns = [
+    path('chef/orders/<int:restaurant_id>', get_chef_orders, name='chief-orders'),
     path('scan/<str:qr_token>/', scan_qr, name='scan_qr'),
     path('', include(router.urls))
 ]
--- backend/my_resto/users/views.py
+++ backend/my_resto/users/views.py
@@ -3,7 +3,9 @@ from rest_framework.viewsets import GenericViewSet
 from restaurants.models import RestoTable
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from orders.models import Order, OrderItem
+from base.choices import OrderStatus
+from datetime import datetime, timedelta, timezone
 # Create your views here.
 @api_view(['GET'])
 def scan_qr(request, qr_token):
@@ -18,3 +20,42 @@ def scan_qr(request, qr_token):
         "restaurant_id": qr.restaurant.id,
         "table_id": qr.id
     })
+
+@api_view(['GET'])
+def get_chef_orders(request, restaurant_id):
+    """
+    Returns active kitchen orders
+    """
+    orders = (
+        Order.objects
+        .filter(
+            restaurant=restaurant_id,
+            status=OrderStatus.PENDING
+        )
+        .select_related('table')
+        .prefetch_related('items__menu_item')
+        .order_by('created_at')
+    )
+
+    res = []
+
+    for order in orders:
+        minutes_ago = int(
+            (datetime.now(timezone.utc) - order.created_at).total_seconds() / 60
+        )
+        items = []
+        for oi in order.items.all():
+            items.append({
+                "item_name": oi.menu_item.name,
+                "quantity": oi.quantity
+            })
+
+        res.append({
+            "order_id": order.id,
+            "table_id": order.table.id,
+            "status": order.status,
+            "time": f"{minutes_ago} min ago",
+            "items": items
+        })
+
+    return Response(res)
